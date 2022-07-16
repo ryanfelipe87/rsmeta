53a2b1488d0999e5dfd772f546a5bbfeb7deb1a5
Ryan <ryanfelipe.rr@gmail.com>
2022-07-16 13:25:46 -0300

    Projeto criado

---
+++ backend/src/main/java/com/ryanf/rsmeta/RsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.ryanf.rsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ryanf/rsmeta/RsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanf.rsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
